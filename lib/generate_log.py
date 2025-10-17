edfd4770bdddb848d69ce3d92c397b0e00aab463
Kaitlyn Brewer <kaitlynmbrewer@gmail.com>
2025-10-16 20:43:10 -0400

    added content from lab

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,16 +1,28 @@
 from datetime import datetime
 import os
 import csv
+import requests
+
+def fetch_data():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    if response.status_code == 200:
+        return response.json()
+    return {}
+
+if __name__ == "__main__":
+    post = fetch_data()
+    print("Fetched Post Title:", post.get("title", "No title found"))
 
 def generate_log(data):
-    # TODO: Implement log generation logic
+    log_data = ["User logged in", "User updated profile", "Report exported"]
 
-    # STEP 1: Validate input
-    # Hint: Check if data is a list
     if isinstance(data,list):
         filename=f"log_{datetime.now().strftime('%Y%m%d')}.csv"
         with open(filename,"w",newline="") as file:
-            writer=csv.writer(file)
+            for entry in log_data:
+                file.write(f"{entry}\n")
+
+        print(f"Log written to {filename}")
 
     # STEP 2: Generate a filename with today's date (e.g., "log_20250408.txt")
     # Hint: Use datetime.now().strftime("%Y%m%d")
